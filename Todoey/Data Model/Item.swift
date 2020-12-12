6584ddc13f386c10fa5073e730f69cea68f85832
Maxim <68609996+squareGTS@users.noreply.github.com>
2020-12-12 08:10:43 +0300

    Create custom data model.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Maxim Bekmetov on 10.12.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var  title : String = ""
+    var done: Bool = false
+    
+}
